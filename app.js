904638218a437ea92d16b3921dabaa4168caab04
AlejandroGutierrez1224 <alejandro.guti062@gmail.com>
2023-09-10 11:23:03 -0500

    Assignment2
    variables

---
+++ app.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+
+var	life = 100 ;//GLOBAL
+life = life -10;
+life = life -10;
+var	name = "AJ" ;// STRING
+
+var box = null; //OBJECT
+console.log(life) ; 
+console.log(name) ;
+console.log(box);
+
+"use strict" ;
+
+(function() { //BLOCK
+let apples = 1;
+if (true) {
+	let	apples = 2;
+console.log(apples);
+}	
+console.log(apples);
+})();
+	
+const i = 2;
+console.log(i);
